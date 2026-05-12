9eb766733b007267cb967df88be76fe3afd635df
Dokito555 <thinkingwhiterabbit@gmail.com>
2026-05-12 23:51:52 +0700

    feat: rewrite everything again..

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,5 @@
 package main
 
-import "github.com/Dokito555/robin/internal/config"
-
 func main() {
-	// init configs
-	viperConfig := config.NewViper()
-	log := config.NewLogger(viperConfig)
-	db := config.NewDatabase(viperConfig, log)
-	validate := config.NewValidator(viperConfig)
-	app := config.NewGin(viperConfig)
 
-	config.Bootstrap(&config.BootstrapConfig{
-		DB:       db,
-		App:      app,
-		Log:      log,
-		Validate: validate,
-		Config:   viperConfig,
-		// KafkaProducer: kafkaProducer,
-	})
-
-	// run app
-	port := viperConfig.GetString("APP_PORT")
-	err := app.Run(":" + port)
-	log.Info("Listening to port: " + port)
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
 }
